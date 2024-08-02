337154a21f27165a085bf4b66904199c21908f7f
kalp26 <kalppandya26@gmail.com>
2024-08-02 19:09:27 +0530

    initial commit

---
+++ done.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=
+    "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+  
+    <link rel="stylesheet" href=
+"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" />
+ 
+    <link rel="stylesheet" href=
+"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="style.css">
+    <title>About Us</title>
+</head>
+<body>
+    <header class="header">
+        <img class="logo" src="groce3.jpg" onclick="" alt="Grocekartlogo" width="160">
+        <nav class="navbar">
+            <a href="grocekart.php">home</a><br>
+            <a href="vegetables.php">vegetables</a><br>
+            <a href="fruits.php">fruits</a><br>
+            <a href="dairy.php">Dairy & Bakery</a><br>
+            <a href="beverages.php">Beverages</a><br>
+            <a href="snacks.php">Snacks</a><br>
+        </nav>
+
+        <div class="icons">
+            <label class="bi bi-menu-button" id="menu-btn"></label>
+            <label class="bi-search" id="search-btn"></label>
+            <label class="bi-cart" id="cart-btn"></label>
+        </div>
+
+        <form action="" class="search-form">
+        <input type="search" id="search-box" placeholder="find here...">
+        <label for="search-box" class="bi-search"></label>
+        </form>
+
+    </header><br><br><br><br><br><br><br><br>
+    <div class="us" id="aboutus">
+        <h1 align="center" style="color: rgb(255, 255, 255);">ABOUT  <img src="groce4.jpg" width="120"></h1>
+    </div><br>
+<br>
+        <div class="us">
+    Indias Favorite Online Shopping Platform
+    </div><br><br>
+    <div class="us">
+        Your payment is done successfully.  <br> Your product will be delivered in next 5-6 working days 
+        <br><br> If you have any problem regarding the product you can contect us on the gmail we will answer you as soon as possible and try to solve your problem.  
+    </div> <br><br><br><br><br><br><br><br><br><br><br>
+
+
+<section class="footer">
+    <div class="box-container">
+
+        <div class="box">
+            <img src="groce2.jpg.png" alt="Grocekart" width="90">      
+            <div class="share">
+                <a href="https://www.instagram.com" class="bi bi-instagram"></a>
+                <a href="https://www.facebook.com" class="bi bi-facebook"></a>
+                <a href="http://www.twitter.com" class="bi bi-twitter"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <h3> contact info</h3>
+            <a href="#" class="link"> <i class="bi bi-telephone"></i>+91-1234567890</a>
+            <a href="#" class="link"> <i class="bi bi-telephone"></i>+91-1223334444</a>
+            <a href="https://www.gmail.com" class="link"> <i class="bi bi-envelope"></i>info@grocekart.com</a>
+            <a href="https://www.gmail.com" class="link"> <i class="bi bi-envelope"></i>customerreview@grocekart.com</a>
+            <a href="#" class="link"> <i class="bi bi-map"></i>Ahmedabad, India - 380008</a>
+        </div>
+
+        <div class="box">
+            <h3> quick links</h3>
+            <a href="grocekart.html" class="link"> <i class="bi bi-arrow-right"></i>Home</a>
+            <a href="#features" class="link"> <i class="bi bi-arrow-right"></i>Features</a>
+            <a href="#topseller" class="link"> <i class="bi bi-arrow-right"></i>Topseller</a>
+            <a href="#categories" class="link"> <i class="bi bi-arrow-right"></i>Categories</a>
+            <a href="#blogs" class="link"> <i class="bi bi-arrow-right"></i>Blogs</a>
+        </div>
+
+        <div class="box">
+            <h3> @copyright-all rights resrved</h3><br><br><br>
+            <a href="#aboutus" class="link"> <i class="bi bi-people"></i>ABOUT US</a>
+        </div>
+
+    </div>
+</section>
+</fieldset>
+</body>
+</html>
